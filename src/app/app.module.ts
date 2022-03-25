fd24c3ecb8c4a73470079354b10f4fcfd93cbb25
Christopher Chantres <chantrex@hotmail.com>
2022-03-24 22:50:02 -0700

    Authentication Login created UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { IndexModule } from './components/index.module';
 import { IndexComponent } from './components/index.component';
+import { LoginComponent } from './auth/components/login/login.component';
 
 
 @NgModule({
@@ -12,7 +14,12 @@ import { IndexComponent } from './components/index.component';
   ],
   imports: [
     BrowserModule,
-    IndexModule
+    IndexModule,
+    RouterModule.forRoot([
+      { path: "", component: IndexComponent },
+      { path: "auth/login", component: LoginComponent },
+      { path: "**", redirectTo: "" }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/index.module.ts
+++ src/app/components/index.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { IndexComponent } from './index.component';
 import { PartialsModule } from './partials/partials.module';
+import { LoginComponent } from '../auth/components/login/login.component';
 
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, PartialsModule],
+  imports: [BrowserModule,
+     FormsModule,
+      PartialsModule,
+       RouterModule],
+
   declarations: [
       IndexComponent
   ],
