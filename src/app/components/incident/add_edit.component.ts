f3d03c647838efe46f419f3f0b395acecc2c3286
AlejandroZhengZheng <ale0720199@gmail.com>
2022-03-31 10:17:57 -0400

    add_edit page

---
+++ src/app/components/incident/add_edit.component.ts
@@ -0,0 +1,46 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Incident } from "../../models/incident.model";
+import { IncidentRepository } from "../../models/incident.repository";
+
+@Component({
+    selector: "add-edit",
+    templateUrl: "add_edit.component.html"
+})
+
+export class AddEditComponent {
+    
+    title:string = 'Add a new Item';
+    editing: boolean = false;
+    item: Incident = new Incident();
+
+    constructor(private repository: IncidentRepository,
+                private router: Router,
+                activeRoute: ActivatedRoute) 
+    { 
+        // Delete
+        if (activeRoute.snapshot.params["mode"] == "delete") {
+            this.deleteItem(activeRoute.snapshot.params["id"]);
+        }
+
+        this.editing = activeRoute.snapshot.params["mode"] == "edit";
+        
+        // Edit
+        if (this.editing) {
+            this.item = this.repository.getItem(activeRoute.snapshot.params["id"]);
+        } 
+
+    }
+
+    save(form: NgForm) {
+        this.repository.saveIncident(this.item);
+        this.router.navigateByUrl("inventory/list");                
+    }
+
+    private deleteItem(id: string){
+        this.repository.deleteIncident(id);
+        this.router.navigateByUrl("inventory/list");
+    }
+    
+}
